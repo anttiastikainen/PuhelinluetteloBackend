ea8466483c5e417d3a91019ece30259f9ea5be30
Antti <antti.astikainen@edu.karelia.fi>
2023-07-14 21:45:53 +0300

    3.16 done

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express()
 const cors = require('cors')
 const Person = require('./models/person')
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('build'))
 
 //get rid of unecessary information of :url
 morgan.token('pathname', (request) => {
@@ -44,26 +44,28 @@ app.get('/info', morgan(tinyFormat), (request, response) => {
     response.end(infoResponse()
     )})
 
-const PORT = process.env.PORT 
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.get('/api/persons/:id', morgan(tinyFormat),(request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', morgan(tinyFormat),(request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
+app.delete('/api/persons/:id',morgan(tinyFormat), (request, response, next) => {
+   Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
     })
-})
-
-app.delete('/api/persons/:id',morgan(tinyFormat), (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !==id)
-
-    response.status(204).end()
+    .catch(error => next(error))
 
 })
 
+/*
 // need to redo this since not using hardcoded persons anymore
 const isDuplicateName = (name) => {
     for(let i = 0;i<persons.length;i++)
@@ -73,7 +75,7 @@ const isDuplicateName = (name) => {
     return false;
 
 }
-
+*/
 const generateId = () => {
     return Math.floor(Math.random() *100000 )
 }
@@ -92,20 +94,38 @@ app.post('/api/persons',morgan(postFormat), (request, response) => {
             error: 'number missing'
         })
     } 
-/*
-    if(isDuplicateName(body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
+    /*
+        if(isDuplicateName(body.name)) {
+            return response.status(400).json({
+                error: 'name must be unique'
+            })
+        }*/ 
+        const person = new Person( {
+            name: body.name,
+            number: body.number,
+            id: generateId(),
         })
-    }*/ 
-    const person = new Person( {
-        name: body.name,
-        number: body.number,
-        id: generateId(),
-    })
 
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if(error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT 
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
 
